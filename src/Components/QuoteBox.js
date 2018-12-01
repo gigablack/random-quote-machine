8370b1c268216dca7abec7e8bb33e7cec13b69cb
Gustavo Bethelmy <tenchu96@gmail.com>
2018-11-30 20:17:04 -0400

    quote machine ready

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,21 +1,59 @@
 import  React, {Component} from 'react'
-import {Typography, Grid,Button,Paper} from '@material-ui/core'
-import {} from '@material-ui/icons'
+import {Typography, Grid,Button} from '@material-ui/core'
+import axios from 'axios'
 
 class QuoteBox extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            quote: {
+                text: '',
+                author: ''
+            }
+        }
+    }
 
+    componentDidMount(){
+        this.fetchQuote()
+    }
+
+    componentDidUpdate(){
+        
+    }
+
+    fetchQuote = async () => {
+        try{
+            this.setState({
+                quote: {
+                    text: '',
+                    author: ''
+                }
+            })
+            const newQuote = await axios.post('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+            this.setState({
+                quote: {
+                    text: newQuote.data.quoteText,
+                    author: newQuote.data.quoteAuthor
+                }
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
     render(){
+        const { author, text} = this.state.quote
         return (
             <div id='quote-box' style={{height:'100%'}}>
                 <Grid container justify='space-evenly' direction='row' alignItems='center' style={{height:'inherit'}}>
                     <Grid item xs={12} style={{marginTop:50}}>
                         <Typography variant='h6' style={{marginLeft:50,marginRight:50}} id='text'>
-                            If one advances confidently in the direction of his dream, and endeavours to live the life which he had imagines, he will meet with a success unexpected in common hours.
+                            {text !== '' ? text : 'loading Quote...'}
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <Typography variant='overline' align='right' style={{marginRight:50}}>
-                            Henry David Thoreau
+                        <Typography variant='overline' align='right' style={{marginRight:50}} id='author'>
+                            {author !== '' ? author : 'loading Author...'}
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
@@ -23,21 +61,25 @@ class QuoteBox extends Component {
                         <Grid item xs={6}>
                             <Grid container justify='space-around'>
                                 <Grid item xs={4}>
+                                   <a id='tweet-quote' href='https://twitter.com/intent/tweet' target='_blank' rel="noopener noreferrer" style={{textDecoration:'none'}}>
                                     <Button variant='contained' color='primary'>
-                                        twitter
+                                        <i className='fab fa-twitter fa-2x'></i>
                                     </Button>
+                                   </a>
                                 </Grid>
                                 <Grid item xs={4}>
-                                    <Button variant='contained' color='primary'>
-                                        github
-                                    </Button>
+                                    <a href='https://github.com/gigablack' target='_blank' rel="noopener noreferrer" style={{textDecoration:'none'}}>
+                                        <Button variant='contained' color='primary'>
+                                            <i className='fab fa-github-alt fa-2x'></i>
+                                        </Button>
+                                    </a>
                                 </Grid>
                             </Grid>
                         </Grid>
                         <Grid item xs={6}>
                             <Grid container justify='flex-end'>
                                 <Grid item xs={5}>
-                                    <Button color='primary' variant='contained'>
+                                    <Button color='primary' variant='contained' id='new-quote' onClick={this.fetchQuote}>
                                         New Quote
                                     </Button>
                                 </Grid>
